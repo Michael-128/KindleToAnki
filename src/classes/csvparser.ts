c840ad60077f94f303394e209f75e5f617aae81e
Michael-128 <michal290704@gmail.com>
2023-12-04 20:41:56 +0100

    Removed console.log

--- src/classes/csvparser.ts
+++ src/classes/csvparser.ts
@@ -3,7 +3,6 @@ export class CSVParser {
 
     static getParsed(headers: string[], values: string[][]) {
         let parsedValues = [headers.join(",")];
-        console.log(values)
         parsedValues = parsedValues.concat(values.map(value => value.join(",")));
         
         const csv = parsedValues.join("\n")
